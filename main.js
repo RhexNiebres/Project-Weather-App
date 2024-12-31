dc8b114aeeec37d3dd7b4f5ff3022494617338d7
RhexNiebres <rhexniebres@gmail.com>
2024-12-30 19:15:12 -0800

    add div to wetherInfo

--- main.js
+++ main.js
@@ -1,45 +1,50 @@
 async function fetchData() {
-    try {
-      const locationName = document
-        .getElementById("locationName")
-        .value.toLowerCase();
+  try {
+    const locationName = document
+      .getElementById("locationName")
+      .value.toLowerCase();
 
-      const response = await fetch(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationName}?unitGroup=us&key=HLWKVM4WWFDXMHUECFVTCY7KE&contentType=json`
-      );
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationName}?unitGroup=us&key=HLWKVM4WWFDXMHUECFVTCY7KE&contentType=json`
+    );
 
-      if (!response.ok) {
-        throw new Error("could not fetch resource");
-      }
-      const data = await response.json();
-      // console.log(data);
-      const currentConditions = data.currentConditions;
+    if (!response.ok) {
+      throw new Error("could not fetch resource");
+    }
+    const data = await response.json();
+    // console.log(data);
+    const currentConditions = data.currentConditions;
 
-      const conditionKey = currentConditions.conditions.toLowerCase();//add 
-      const imageResponse = await fetch(
-        `https://api.giphy.com/v1/gifs/translate?api_key=kjslOcjrBfTc6PDM4KhjjRSxBgfl7HkG&s=${conditionKey}`
-      );
+    const conditionKey = currentConditions.conditions.toLowerCase(); //add
+    const imageResponse = await fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=kjslOcjrBfTc6PDM4KhjjRSxBgfl7HkG&s=${conditionKey}`
+    );
 
-      if (!imageResponse.ok) {
-        throw new Error("Could not fetch GIF resource");
-      }
-      const imageData = await imageResponse.json();
-      const imageUrl = imageData.data.images.original.url;
+    if (!imageResponse.ok) {
+      throw new Error("Could not fetch GIF resource");
+    }
+    const imageData = await imageResponse.json();
+    const imageUrl = imageData.data.images.original.url;
 
-      const display = document.getElementById("display");
+    const display = document.getElementById("display");
 
-      const weatherInfo = `
-      <h3>Wheather in ${locationName}</h3>
-      <p><i class="fa-solid fa-cloud-sun"></i> ${currentConditions.conditions}</p>
-      <p><i class="fa-solid fa-temperature-half"></i> ${currentConditions.temp}</p>
-      <p><i class="fa-solid fa-droplet"></i> ${currentConditions.humidity}</p>
-      <img src="${imageUrl}" alt="Image of ${currentConditions.temp}" />
+    const weatherInfo = `
+      <div class="weather-info">
+       <div>
+         <h3>Wheather in ${locationName}</h3>
+         <p><i class="fa-solid fa-cloud-sun"></i> ${currentConditions.conditions}</p>
+         <p><i class="fa-solid fa-temperature-half"></i> ${currentConditions.temp}</p>
+         <p><i class="fa-solid fa-droplet"></i> ${currentConditions.humidity}</p>
+       </div>
+       <div>
+         <img src="${imageUrl}" alt="Image of ${currentConditions.temp}" />
+       </div>
+      </div>
       `;
 
-      display.innerHTML = weatherInfo;
-      display.style.display = "block";
-      
-    } catch (error) {
-      console.log(error);
-    }
+    display.innerHTML = weatherInfo;
+    display.style.display = "block";
+  } catch (error) {
+    console.log(error);
   }
+}
